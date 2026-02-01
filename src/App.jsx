b4021a1f353389291a904d3677f225a8ece4d418
Mayochia <22100120@usc.edu.ph>
2026-02-01 13:33:42 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/nav-bar.jsx'
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <div>
+        <p>what's up madafaka</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav-bar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <h1>My Portfolio</h1>
+    </nav>
+  )
+}
